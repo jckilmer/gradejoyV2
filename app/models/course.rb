81e216fb981a04816bb1e7d1b095ca3ee91be02b
Robert Landreaux <rcl2288@yahoo.com>
2015-07-17 09:38:23 -0400

    add creating course feature

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,5 @@
 class Course < ActiveRecord::Base
   belongs_to :user
   validates :name, presence: true
+  validates :user, presence: true
 end
--- spec/support/pages/courses_index_page.rb
+++ spec/support/pages/courses_index_page.rb
@@ -7,4 +7,18 @@ class CourseIndexPage < Page
   def has_course?(course)
     has_css?(".qa-course-#{course.id}")
   end
+
+  def click_create_new_course_button
+    find('.qa-create-course').click
+  end
+
+  def fill_form(attributes)
+    find('.qa-course-name').set(attributes[:name])
+    find('.qa-course-period').set(attributes[:period])
+    find('.qa-course-section').set(attributes[:section])
+  end
+
+  def submit
+    find('.qa-submit-course').click
+  end
 end
